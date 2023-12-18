916e4afae9d25078f7906845097a2e4903febc52
Luana Mattana Damin <ludamin@gmail.com>
2023-12-18 10:53:29 -0300

    textFieldDelegate Register

--- brasil/brasil/Screens/RegisterVC/RegisterVC.swift
+++ brasil/brasil/Screens/RegisterVC/RegisterVC.swift
@@ -7,23 +7,58 @@
 
 import UIKit
 
-class RegisterVC: UIViewController {
-
+class RegisterVC: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var createAccountLabel: UILabel!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var nacionalityTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var registerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configElements()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configElements() {
+        
+        nameTextField.placeholder = "Your full name"
+        nameTextField.delegate = self
+        
+        nacionalityTextField.placeholder = "Your full name"
+        nacionalityTextField.delegate = self
+        
+        emailTextField.placeholder = "Your e-mail Address"
+        emailTextField.keyboardType = .emailAddress
+        emailTextField.delegate = self
+        
+        passwordTextField.placeholder = "Your Password"
+        passwordTextField.delegate = self
+        passwordTextField.isSecureTextEntry = true
+        
+        createAccountLabel.text = "Create Your Account"
+        
+        registerButton.setTitle("Register", for: .normal)
     }
-    */
-
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.layer.borderWidth = 1.0
+        textField.layer.borderColor = UIColor.systemBlue.cgColor
+        
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.layer.borderWidth = 0
+        textField.layer.borderColor = UIColor.gray.cgColor
+    }
 }
+
+
+
