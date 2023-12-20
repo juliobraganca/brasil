039238dfafca6b15df13a12572eaaba50927a313
Eduardo <eduardo07assis@gmail.com>
2023-12-20 18:05:50 -0300

    inserindo método de cadastro + Alert's

--- brasil/brasil/Screens/ForgotPasswordVC/ForgotPasswordVC.swift
+++ brasil/brasil/Screens/ForgotPasswordVC/ForgotPasswordVC.swift
@@ -76,7 +76,7 @@ extension ForgotPasswordVC: UITextFieldDelegate {
 }
 
 extension UITextField {
-    func validateEmail() -> Bool {
+    func validateEmaill() -> Bool {
         let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let validateRegex = NSPredicate(format: "SELF MATCHES %@", emailRegex)
         return validateRegex.evaluate (with: self.text)
--- brasil/brasil/Screens/RegisterVC/RegisterVC.swift
+++ brasil/brasil/Screens/RegisterVC/RegisterVC.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import Firebase
+
+
 
 class RegisterVC: UIViewController, UITextFieldDelegate {
     
@@ -21,8 +24,13 @@ class RegisterVC: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var registerButton: UIButton!
     
+    var auth: Auth?
+    var alert: Alert?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.auth = Auth.auth()
+        self.alert = Alert(controller: self)
         configElements()
         
     }
@@ -48,6 +56,22 @@ class RegisterVC: UIViewController, UITextFieldDelegate {
         registerButton.setTitle("Register", for: .normal)
     }
     
+    
+    @IBAction func tappedRegisterButton(_ sender: Any) {
+        let email: String = self.emailTextField.text ?? ""
+        let password: String = self.passwordTextField.text ?? ""
+        auth?.createUser(withEmail:email, password: password, completion: { (Result, error) in
+            if error != nil {
+                self.showAlert(title: "Atenção", message: "Erro ao cadastrar. Verifique seus dados")
+            } else{
+                self.ShowAlertReturnLogin(title:"Parabéns", message: "Registro realizado com sucesso!")
+                
+            }
+        })
+        
+    }
+    
+    
     func textFieldDidBeginEditing(_ textField: UITextField) {
         textField.layer.borderWidth = 1.0
         textField.layer.borderColor = UIColor.systemBlue.cgColor
@@ -58,7 +82,50 @@ class RegisterVC: UIViewController, UITextFieldDelegate {
         textField.layer.borderWidth = 0
         textField.layer.borderColor = UIColor.gray.cgColor
     }
+    
+    
+    
+    func showAlert(title:String, message: String) {
+        let alertController: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okButton: UIAlertAction = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(okButton)
+        self.present(alertController,animated: true, completion: nil)
+    }
+    
+    func ShowAlertReturnLogin(title: String, message: String){
+        let alertController: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertControllerReturnLogin: UIAlertAction = UIAlertAction(title: "OK", style: .default) {(action) in
+            self.redirectForLogin()
+        }
+        alertController.addAction(alertControllerReturnLogin)
+        self.present(alertController,animated: true, completion: nil)
+    }
+    
+    func redirectForLogin(){
+        self.navigationController?.pushViewController(TabBarController(), animated: true)
+    }
+    
 }
+
+extension UITextField {
+func validateEmail() -> Bool {
+    let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    let validateRegex = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+    return validateRegex.evaluate (with: self.text)
+}
+func validatePassword() -> Bool {
+    let passwordRegex = ".{6,}"
+    let validateRegex = NSPredicate (format: "SELF MATCHES %@", passwordRegex)
+    return validateRegex.evaluate(with: self.text)
+    
+}
+func validateName() -> Bool {
+    let passwordRegex = "^[a-zA-Z]{2,}$"
+    let validateRegex = NSPredicate (format: "SELF MATCHES %@", passwordRegex)
+    return validateRegex.evaluate(with: self.text)
+}
+}
+
 
 
 
